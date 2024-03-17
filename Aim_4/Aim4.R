2ee2b55162722176b4dee7e36d14122cb4625697
adamvoss10 <158863192+adamvoss10@users.noreply.github.com>
2024-03-17 15:47:06 -0700

    Update Aim4.R

--- Aim_4/Aim4.R
+++ Aim_4/Aim4.R
@@ -16,16 +16,16 @@ highlow_genus <- tax_glom(pd_final, "Genus", NArm = FALSE)
 #convert to relative abundance
 highlow_genus_RA <- transform_sample_counts(highlow_genus, fun=function(x) x/sum(x))
 
-#generate file for nutrient omitting NA
-highlow_genus_RA_starchomit <- subset_samples(highlow_genus_RA, !is.na(sample_data(highlow_genus_RA)[["Non_Starch_Polysaccharides_highlow"]]))
-
-
-isa_highlow_nonstarchpolysaccharides <- multipatt(t(otu_table(highlow_genus_RA_starchomit)), cluster = sample_data(highlow_genus_RA_starchomit)$`Non_Starch_Polysaccharides_highlow`)
-summary(isa_highlow_nonstarchpolysaccharides)
-taxtable <- tax_table(pd_rare) %>% as.data.frame() %>% rownames_to_column(var="ASV")
-isa_highlow_nonstarchpolysaccharides$sign %>%
-  rownames_to_column(var="ASV") %>%
-  left_join(taxtable) %>%
-  filter(p.value<0.05) %>% View()
+#generate file for nutrient omitting NA for each nutrient
+for (nutrient in sig_nutrient) { #for each nutrient in the sig_nutrient category
+  subset_data <- subset_samples(highlow_genus_RA, !is.na(sample_data(highlow_genus_RA)[[nutrient]])) #remove all of the NA samples
+  isa_subset <- multipatt(t(otu_table(subset_data)), cluster = sample_data(subset_data)[[nutrient]]) #running the isa analysis on the samples without NA
+  taxtable <- tax_table(pd_final) %>% as.data.frame() %>% rownames_to_column(var="ASV") #extract row of taxa table from phyloseq object and convert to new row
+  isa_subset_table <- isa_subset$sign %>% #save the table in a new object
+    rownames_to_column(var="ASV") %>% #make the ASV rows column names in isa_subset_table
+    left_join(taxtable) %>% #leftjoin and align
+    filter(p.value<0.05) #filter from the asv table to remove non signficant
+  write.csv(isa_subset_table, file = paste0(nutrient, "_filtered_table.csv"), row.names = FALSE) #save the new file as a csv
+}
 
 
